9d600fb78b2f62e9f3bc651d02247e8150c46228
Packerfan-Gamer <packerfangamer@gmail.com>
2017-04-13 20:07:46 -0500

    Update LegacyCOOLmod1.js

--- LegacyCOOLmod1.js
+++ LegacyCOOLmod1.js
@@ -10,7 +10,16 @@ manifest:'ModManifest.js',
 requires:['Default dataset*'],
 sheets:{'imageSheet':'http://i.imgur.com/pYwTikq.png'},//custom stylesheet (note : broken in IE and Edge for the time being)
 func:function()
-/*{
+	{	
+		new G.Res({
+		name:'Fruit Juice',
+		desc:'[Fruit Juice] tastes better than [water].',
+		icon:[0,1,'imageSheet'],
+		turnToByContext:{'eat':{'health':0.06,'happiness':0.1},'decay':{'spoiled food':0.2}},//this basically translates to : "when eaten, generate some health and happiness; when rotting, turn into either nothing or some spoiled food"
+		partOf:'food',
+		category:'food',
+		}
+/*
 	//mod to add berries and juice
 	
 	//First we add the new resources 
